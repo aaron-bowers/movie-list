8f25f05c9c3d4c550abda8f6c5ea31f14dc039d5
Aaron Bowers <bowersaaronjames@gmail.com>
2022-04-27 17:23:53 -0700

    add a search bar and sorted function

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,45 @@
 import React from 'react';
+import Search from './Search.jsx';
+import MovieList from './MovieList.jsx';
 
-const App = (props) => (
-  <div>Hello World!</div>
-);
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      value: '',
+      movieList: ['Mean Girls', 'Hackers', 'The Grey', 'Sunshine', 'Ex Machina'],
+      sorted: []
+    }
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(userInput) {
+      const movieListCopy = this.state.movieList.slice();
+      const filteredList = movieListCopy.filter((movie) => {
+        if (movie.toLowerCase().includes(userInput.toLowerCase())) {
+          return movie;
+        }
+      })
+      this.setState({
+        value: userInput,
+        sorted: filteredList
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className="header">MovieList</h2>
+        <Search
+          value={this.state.value}
+          handleGo={this.handleSubmit}
+        />
+        {this.state.sorted.length > 0 ?
+          <MovieList list={this.state.sorted}/> :
+          <MovieList list={this.state.movieList}/>}
+      </div>
+    );
+  }
+}
 
 export default App;
